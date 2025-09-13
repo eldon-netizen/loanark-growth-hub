0ddf1f6eac97ebdd64b87e4e8386b65731a55e48
eldon-netizen <eldon@loanark.com>
2025-09-13 10:27:20 +0800

    Add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/loanark-growth-hub">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/flyers" element={<Flyers />} />
