4d5a0e9c1f022346da609425a14ebf61f836b7cf
eldon-netizen <eldon@loanark.com>
2025-09-13 08:21:04 +0800

    Reduce image container size by 2px

--- src/components/ui/services-section.tsx
+++ src/components/ui/services-section.tsx
@@ -50,7 +50,7 @@ const ServicesSection = () => {
               <CardHeader className="text-center p-0 flex-1 flex flex-col">
                 <div className="w-full h-96 flex items-center justify-center overflow-hidden">
                   <div className="w-full h-full">
-                    <div className="w-full h-full scale-[1.5] flex items-center justify-center">
+                    <div className="w-full h-full scale-[1.48] flex items-center justify-center">
                       {service.icon}
                     </div>
                   </div>
